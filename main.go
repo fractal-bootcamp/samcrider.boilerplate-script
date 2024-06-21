bc45e424f66920fd78d8e1320b77d9c43283445c
SamuelRCrider <sam.crider@aol.com>
2024-06-21 15:40:18 -0400

    script now creates project as a monorepo with git initialized

--- main.go
+++ main.go
@@ -4,39 +4,59 @@ package main
 
 import (
 	"fmt"
+	"os"
 
-	"github.com/AlecAivazis/survey/v2"
 	express_boil "sam.crider/boilerplate-script/express"
+	"sam.crider/boilerplate-script/utils"
 	vite_boil "sam.crider/boilerplate-script/vite"
 )
 
-func Checkboxes(label string, opts []string) string {
-    res := ""
-    prompt := &survey.Select{
-        Message: label,
-        Options: opts,
-    }
-    survey.AskOne(prompt, &res)
-
-    return res
-}
-
 func main() {
-    answer := Checkboxes(
-        "Select Your Build Stack:",
-        []string{
-            "Vite + Express",
+	// get the user's selected stack
+	stack := utils.Select_Stack(
+		"Select Your Build Stack:",
+		[]string{
+			"Vite + Express",
 			"Next.js",
-        },
-    )
+		},
+	)
 
-	if answer == "Vite + Express" {
+	// get the user's project name
+	project_name := utils.Name_Project(
+		"What's the name of this project?",
+	)
+
+	if stack == "Vite + Express" {
+		// create a directory for the project, 0755 is the permission bits
+		err := os.Mkdir(project_name, 0755)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		// cd into the new project
+		err = os.Chdir(project_name)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		// initialize git for the project
+		cmd := utils.BoundCommand("git", "init")
+		if err := cmd.Run(); err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		// create the frontend
 		vite_boil.Vite()
+
+		// create the backend
 		express_boil.Express()
+
 		fmt.Println("Success! Boilerplate created.")
 		return
 	}
-	if answer == "Next.js + " {
+	if stack == "Next.js" {
 		fmt.Println("Coming Soon")
 		fmt.Println("Success! Boilerplate created.")
 		return
--- utils/utils.go
+++ utils/utils.go
@@ -3,12 +3,15 @@ package utils
 import (
 	"os"
 	"os/exec"
+
+	"github.com/AlecAivazis/survey/v2"
 )
 
 
 func BoundCommand(name string, arg ...string) (*exec.Cmd) {
 	command := exec.Command(name, arg...)
 
+	// bind command to terminal
 	command.Stdin = os.Stdin
 	command.Stdout = os.Stdout
 	command.Stderr = os.Stderr
@@ -17,7 +20,28 @@ func BoundCommand(name string, arg ...string) (*exec.Cmd) {
 	return command
 }
 
-func GenerateConstantsFromFiles (){
-	// crawl through the directory and transform *.constant.txt files to constant string arrays
-	// parse those files and write them to a new .generated.go file
+func Select_Stack(label string, opts []string) string {
+	res := ""
+	prompt := &survey.Select{
+		Message: label,
+		Options: opts,
+	}
+	survey.AskOne(prompt, &res)
+
+	return res
 }
+
+func Name_Project(label string) string {
+	res := ""
+	prompt := &survey.Input{
+		Message: label,
+	}
+	survey.AskOne(prompt, &res)
+
+	return res
+}
+
+// func GenerateConstantsFromFiles (){
+// 	// crawl through the directory and transform *.constant.txt files to constant string arrays
+// 	// parse those files and write them to a new .generated.go file
+// }
